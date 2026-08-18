871885ef0e0fd65a1a850941950524297e511f9d
solaiman844 <solaimaniiisarip@gmail.com>
2026-08-18 14:11:53 +0800

    Remove_app_pages_and_components_keep_landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link as RouterLink } from 'react-router-dom';
 import { Box, Flex, HStack, Spacer, Button, Heading, Text } from '@chakra-ui/react';
-import ClientManagement from './pages/ClientManagement/ClientManagement';
 import Landing from './pages/Landing/Landing';
 
 const App: React.FC = () => {
@@ -16,14 +15,12 @@ const App: React.FC = () => {
           <Spacer />
           <HStack spacing={3}>
             <Button as={RouterLink} to="/" variant="ghost">Home</Button>
-            <Button as={RouterLink} to="/clients" colorScheme="blue">Clients</Button>
           </HStack>
         </Flex>
       </Box>
       <main style={{ padding: 16 }}>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/clients" element={<ClientManagement />} />
           <Route path="*" element={<Landing />} />
         </Routes>
       </main>
