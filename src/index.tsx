d7acc4a3363092a07345c791e3b079e5fa9efd4e
solaiman844 <solaimaniiisarip@gmail.com>
2026-08-18 13:35:04 +0800

    Fix_font_import_use_index_css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './styles/global.css';
-import '@fontsource/inter/variable.css';
+import '@fontsource/inter/index.css';
 import theme from './theme';
 
 const container = document.getElementById('root');
